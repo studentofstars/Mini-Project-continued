5117eaf54796b453e42c217c460c4a7e8ca280e6
Mrutyunjaya Muduli <mudulimrutyunjaya42@gmail.com>
2025-05-14 15:06:52 +0530

    Update Drought_Prediction_App.py

--- Drought_Prediction_App.py
+++ Drought_Prediction_App.py
@@ -12,7 +12,7 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 st.set_page_config(page_title="Drought Prediction App", layout="wide")
 st.title("🌾 Drought Phase Prediction using Random Forest")
 
-uploaded_file = http://raw.githubusercontent.com/studentofstars/Mini-Project-continued/main/data_labels_cleaned.csv
+uploaded_file = "http://raw.githubusercontent.com/studentofstars/Mini-Project-continued/main/data_labels_cleaned.csv"
 
 if uploaded_file:
     df = pd.read_csv(uploaded_file)
